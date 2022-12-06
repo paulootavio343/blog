b60caccde4569b300e279bd0f79574aebdbd5d1f
Paulo Otávio <92699268+paulootavio343@users.noreply.github.com>
2022-12-06 02:19:00 -0300

    Fix comment form design

--- blog_content/forms.py
+++ blog_content/forms.py
@@ -1,8 +1,23 @@
 from django.forms import ModelForm
 from .models import Comentaries
+from django import forms
 
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comentaries
         fields = ('name', 'email', 'title', 'message')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control mb-2',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control mb-2',
+            }),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control mb-2',
+            }),
+            'message': forms.Textarea(attrs={
+                'class': 'form-control',
+            }),
+        }
